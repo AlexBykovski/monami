50c47f74b80b125b4843ea5bc68e3fa94514f43d
AlexBykovski <bykovski.work@gmail.com>
2018-07-21 23:58:12 +0300

    add creating of promocode

---
+++ src/Migrations/2018/Version20180721205400.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180721205400 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE promo_code CHANGE code code VARCHAR(191) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_3D8C939E77153098 ON promo_code (code)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_3D8C939E77153098 ON promo_code');
+        $this->addSql('ALTER TABLE promo_code CHANGE code code VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}

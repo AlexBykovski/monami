33b26923175671d62a2ad6aef2319641e3943298
AlexBykovski <bykovski.work@gmail.com>
2018-10-02 13:54:04 +0300

    add pagination

--- assets/js/client/controller/filter-products-controller.js
+++ assets/js/client/controller/filter-products-controller.js
@@ -7,11 +7,13 @@
         let idGroup = "";
         this.products = [];
         this.maxCount = Number.MAX_SAFE_INTEGER;
+        this.countPages = Number.MAX_SAFE_INTEGER;
         this.products = [];
         this.filters = {
             sort: "cost",
             count: 16,
-            text: ""
+            text: "",
+            page: 1
         };
 
         function init(idGroupS, textS) {
@@ -36,13 +38,26 @@
                 url: url,
                 params: self.filters
             }).then(function successCallback(response) {
-                self.products = response.data;
+                self.products = response.data.products;
+                self.countPages = response.data.countPages;
             }, function errorCallback(error) {
                 console.error(error);
             });
         }
 
+        function toPage(page) {
+            console.log(page);
+            if(page < 1 || page > this.countPages){
+                return false;
+            }
+
+            this.filters.page = page;
+
+            updateProducts();
+        }
+
         this.updateProducts = updateProducts;
+        this.toPage = toPage;
         this.init = init;
         }]);
 })(window.appMonami);
